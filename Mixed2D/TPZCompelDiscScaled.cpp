267ef90f371ee0c27cc6b6fcb59ac60f9d7db41f
philippedevloo <philippedevloo1958@gmail.com>
2018-05-03 19:28:47 -0300

    Class skeleton to make the tensor formulation independent of element size

---
+++ Mixed2D/TPZCompelDiscScaled.cpp
@@ -0,0 +1,8 @@
+//
+//  TPZCompelDiscScaled.cpp
+//  MixedElasticityGirkmann
+//
+//  Created by Philippe Devloo on 03/05/18.
+//
+
+#include "TPZCompelDiscScaled.hpp"
---
+++ Mixed2D/TPZCompelDiscScaled.hpp
@@ -0,0 +1,13 @@
+//
+//  TPZCompelDiscScaled.hpp
+//  MixedElasticityGirkmann
+//
+//  Created by Philippe Devloo on 03/05/18.
+//
+
+#ifndef TPZCompelDiscScaled_hpp
+#define TPZCompelDiscScaled_hpp
+
+#include <stdio.h>
+
+#endif /* TPZCompelDiscScaled_hpp */
