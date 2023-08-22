ec55686470cca2b729e9937a910d0fce10b89280
PhilippeDevloo <phil@fec.unicamp.br>
2023-08-22 16:56:15 -0300

    Classes to restrain the voronoi shape functions

---
+++ MHM-Elas-3D/VoronoiAnalise.cpp
@@ -0,0 +1,8 @@
+//
+//  VoronoiAnalise.cpp
+//  mhm-elas-3d
+//
+//  Created by Philippe Devloo on 21/08/23.
+//
+
+#include "VoronoiAnalise.hpp"
---
+++ MHM-Elas-3D/VoronoiAnalise.h
@@ -0,0 +1,13 @@
+//
+//  VoronoiAnalise.hpp
+//  mhm-elas-3d
+//
+//  Created by Philippe Devloo on 21/08/23.
+//
+
+#ifndef VoronoiAnalise_hpp
+#define VoronoiAnalise_hpp
+
+#include <stdio.h>
+
+#endif /* VoronoiAnalise_hpp */
